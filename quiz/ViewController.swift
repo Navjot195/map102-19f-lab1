535d342cdc50f476661f9f674747e044ee065d21
Navjot Kaur <19039495@g200022-19f.local>
2019-09-06 14:20:45 -0400

    Initial Commit

---
+++ quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  quiz
+//
+//  Created by Navjot Kaur on 2019-09-06.
+//  Copyright © 2019 Navjot Kaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
